1bda4558f164c52184c9f72c38d9a62d9d7ad653
AlexZhenin <alexeyzhenin@mail.ru>
2023-06-25 08:45:35 +0500

    Создан класс Move (перемещение)

---
+++ Models/Move.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maps.Models
+{
+    class Move
+    {
+        public string ID { get; set; }
+        public double[] Coordinates { get; set; } = new double[]
+        {
+            new double[] { 0, 0 }, // Широта и долгота
+        };
+        public DateTime Time { get; set; }
+    }
+}
