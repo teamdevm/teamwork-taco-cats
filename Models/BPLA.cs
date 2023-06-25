89d994a35d88b47aff6c32743f90f18fa6a728ed
AlexZhenin <alexeyzhenin@mail.ru>
2023-06-25 08:49:50 +0500

    Создан класс BPLA (БПЛА)

---
+++ Models/BPLA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maps.Models
+{
+    class BPLA
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        ObservableCollection<Move> CollectionOfMove = new ObservableCollection<Move>();
+    }
+}
