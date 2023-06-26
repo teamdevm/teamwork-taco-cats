4f4994a4eb1ee9aee07d497b4050ac4385c57471
JustRostok <rostislav.nizametdinov@mail.ru>
2023-06-26 15:34:27 +0500

    Создание и первичное заполнение списка объектов

--- Models/CollectionOfBPLA.cs
+++ Models/CollectionOfBPLA.cs
@@ -7,22 +7,21 @@ using System.Threading.Tasks;
 
 namespace Maps.Models
 {
-    class CollectionOfBPLA
+    public class CollectionOfBPLA
     {
-        ObservableCollection<BPLA> CollectionOfBPLA = ObservableCollection<BPLA>();
+        ObservableCollection<BPLA> CollectionOfBPLA { get; set; } = ObservableCollection<BPLA>();
         public void create()
         {
 
-            CollectionOfBPLA.Add(
-            new BPLA
+            CollectionOfBPLA.Add(new BPLA
             {
                 ID = "1",
                 Name = "Test object №1",
                 Description = "Test object №1",
-                CollectionOfMove.Add(new Move { "1", new double[] { 58.00711, 56.18835 }, (2023, 6, 20, 18, 30, 25)}, 
-                                     new Move { "1", new double[] { 58.01587, 56.24571 }, (2023, 6, 20, 18, 35, 25)})                
-            },
-            new BPLA
+                CollectionOfMove.Add(new Move { "1", new double[] { 58.00711, 56.18835 }, (2023, 6, 20, 18, 30, 25) },
+                                     new Move { "1", new double[] { 58.01587, 56.24571 }, (2023, 6, 20, 18, 35, 25) })
+            });
+            CollectionOfBPLA.Add(new BPLA
             {
                 ID = "2",
                 Name = "Test object №2",
@@ -30,8 +29,7 @@ namespace Maps.Models
                 CollectionOfMove.Add(new Move { "2", new double[] { 58.05427, 56.41754 }, (2023, 6, 20, 18, 30, 25) },
                                      new Move { "2", new double[] { 58.06807, 56.55899 }, (2023, 6, 20, 18, 35, 46) })
             });
-
-
+            return CollectionOfBPLA;
         }
     }
 }
