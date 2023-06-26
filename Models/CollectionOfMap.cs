e59e92a0688ba5ec5748d410c9f509842b0efc9a
a.starikov <NixAtis@outlook.com>
2023-06-26 15:59:51 +0500

    Работа с Коллекций карт

--- Models/CollectionOfMap.cs
+++ Models/CollectionOfMap.cs
@@ -1,16 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Avalonia.Media.Imaging;
 
 namespace Maps.Models
 {
     public class CollectionOfMap
     {
-        public void Create()
-        {
+        private ObservableCollection<Map> _CollectionOfMap = new ObservableCollection<Map>();
 
+        public ObservableCollection<Map> Create()
+        {
+            
+            _CollectionOfMap.Clear();
+
+            string _workingDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            var folderPath = $"{_workingDirectory}\\Assets\\Maps";
+            using var fileStream = new FileStream(Path.Combine(folderPath, "TestImage.png"), FileMode.Open, FileAccess.Read) { Position = 0 };
+            var bitmap = new Bitmap(fileStream);
+
+
+            _CollectionOfMap.Add(new Map
+            {
+                ID = "0",
+                Name = "Test Map",
+                Description = "Test Map",
+                Coordinates = new double[][]
+                {
+                    new double[] { 58.074246, 54.664838 },
+                    new double[] { 57.629111, 54.645612 },
+                },
+                FilePath = Path.Combine(folderPath, "TestImage.png"),
+                Image = bitmap
+            });
+
+            return _CollectionOfMap;
         }
     }
 }
--- Models/Model.cs
+++ Models/Model.cs
@@ -31,33 +31,7 @@ namespace Maps.Models
         [JsonIgnore]
         public Bitmap Image { get; set; }
     }
-
-    public class CollectionMap
-    {
-        ObservableCollection<Map> CollectionOfMap = new ObservableCollection<Map>();
-        public void Create()
-        {
-            string _workingDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
-            var folderPath = $"{_workingDirectory}\\Assets\\Maps";
-            using var fileStream = new FileStream(Path.Combine(folderPath, "TestImage.png"), FileMode.Open, FileAccess.Read) { Position = 0 };
-            var bitmap = new Bitmap(fileStream);
-
-
-            CollectionOfMap.Add(new Map
-            {
-                ID = "0",
-                Name = "Test Map",
-                Description = "Test Map",
-                Coordinates = new double[][]
-                {
-                    new double[] { 58.074246, 54.664838 },
-                    new double[] { 57.629111, 54.645612 },
-                },
-                FilePath = Path.Combine(folderPath, "TestImage.png"),
-                Image = bitmap
-            });
-        }
-    }
+    
 
 
 }
